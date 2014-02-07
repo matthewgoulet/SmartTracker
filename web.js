ecf42ef5ec9f5c15d581273f06ffdfadcbebd2f0
Matthew Goulet <matthew.goulet@mail.mcgill.ca>
2014-02-07 18:27:13 -0500

    Added mongoDB

--- web.js
+++ web.js
@@ -1,6 +1,19 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+	process.env.MONGOHQ_URL ||
+	'mongodb://<heroku_app22012954>:<SmartTracker1>@ds027509.mongolab.com:27509/heroku_app22012954';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+	db.collection('mydocs', function(er, collection) {
+		collenction.insert({'mykey': 'vLXd7kNkFPShIejeAuQRqmQCEaMCambU'}, {safe: true}, function(er,rs) {
+		});
+	});
+});
+
 
 app.use(logfmt.requestLogger());
 
