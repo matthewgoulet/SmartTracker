39943312242bb5389c183fc297faf548dfb572f3
Matthew Goulet <matthew.goulet@mail.mcgill.ca>
2014-02-07 18:09:20 -0500

    Changed hello world text again

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-	res.send('Hello Worldlings!');
-});
+	res.send('Hello Worldlings! Still trying to test this!!');
+};
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
