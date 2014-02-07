d9b0529b4dea80b4f2e1245ea9c5e06f10cdc1a4
Matthew Goulet <matthew.goulet@mail.mcgill.ca>
2014-02-07 18:16:09 -0500

    Testing if i can push to two repos

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-	res.send('Hello Worldlings! Still trying to test this!!');
+	res.send('Hello World! Still trying to test this!!');
 });
 
 var port = Number(process.env.PORT || 5000);
