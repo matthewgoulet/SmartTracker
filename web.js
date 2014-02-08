cdfeaa38add3b99ec7ebcdc29082dd9a52188590
Matthew Goulet <matthew.goulet@mail.mcgill.ca>
2014-02-07 22:21:32 -0500

    Added mongoDB with mongoose

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var logfmt = require("logfmt");
 var app = express();
 var mongo = require('mongodb');
 var redis = requre('redis-url').connect(process.env.REDISTOGO_URL);
+var mongoose = require('mongoose');
 
 redis.set('foo', 'bar');
 
@@ -15,7 +16,7 @@ var mongoUri = process.env.MONGOLAB_URI ||
 	process.env.MONGOHQ_URL ||
 	'mongodb://heroku_app22012954:SmartTracker1@ds027509.mongolab.com:27509/heroku_app22012954';
 
-mongo.Db.connect(mongoUri, function (err, db) {
+mongoose.connect(mongoUri, function (err, db) {
 	db.collection('mydocs', function(er, collection) {
 		collection.insert({'mykey': 'vLXd7kNkFPShIejeAuQRqmQCEaMCambU'}, {safe: true}, function(er,rs) {
 		});
